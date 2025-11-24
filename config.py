9d3d998d5cdb2f8195c482960a95258379659faa
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-11-24 15:38:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     LEADERBOARD_DELETE_TIMER = 30
     # wes response configuration     
-    WEBHOOK = os.environ.get("WEBHOOK", "False").lower() == "true"
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 
     
     #========================================================================================   
